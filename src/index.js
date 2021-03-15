9e88185a01165651ce9bdb5f16b31e0202f7767f
Tatiana Sender <tatsiana_sender@mail.ru>
2021-03-15 23:40:05 +0300

    add part of solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let brackets = "[]{}()<>";
+  let stack = [];
+
+
+  for(let bracket of str) {
+    let bracketsIndex = brackets.indexOf(bracket);
+    if(bracketsIndex % 2 === 0) {
+      stack.push(bracketsIndex + 1)
+    } else {
+      stack.pop();
+    }
+  }
+  return stack.length == 0 ? true : false;
+};
